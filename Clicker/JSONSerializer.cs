b64e458a5426cac31e62a82a8fa19e75cb56450c
Daniel Radu <danyhk94@gmail.com>
2020-04-02 10:06:59 +0300

    Clean JSONSerializer

--- Clicker/JSONSerializer.cs
+++ Clicker/JSONSerializer.cs
@@ -7,12 +7,8 @@ namespace Clicker
     {
         public static void SerializeScenario(Scenario scenario)
         {
-            // serialize JSON directly to a file
             using (StreamWriter file = File.CreateText(@"scenario_1.json"))
             {
-                //JsonSerializer serializer = new JsonSerializer();
-                //serializer.Serialize(file, scenario);
-
                 string json = JsonConvert.SerializeObject(scenario, Formatting.Indented, new ActionsJsonConverter());
                 file.Write(json);
             }
